e1d570e1277c58020de2325a402461803123e53b
vova <vova.lev.16@gmail.com>
2018-11-03 18:15:00 +0200

    технічний коміт після підключення до репозиторію

---
+++ C-sharp_lab_Volodymyr_Levchuk_IS63/Lab_1/Lab_1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+    abstract class SystemCoder
+    { 
+        public string name {get;set;}
+        public SystemCoder(string name)
+        {
+            this.name = name;
+        }
+        abstract public Coder Decode(string data);
+        abstract public Coder Encode(string data);
+    }
+    abstract class Coder
+    {  }
+
+    class FirstTypeCoder
+    {
+     
+    }
+
+    class ShiftCoder : Coder
+    {
+        private string data;
+
+        public void Decode(string data)
+        {
+
+        }
+
+        public void Encode(string data)
+        {
+
+        }
+    }
+}
